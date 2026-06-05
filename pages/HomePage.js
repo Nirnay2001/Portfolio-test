ee6d39c8af516952d0cfa2c4d84b4ece4650f866
Nirnay <sealnirnay5@gmail.com>
2026-06-05 22:41:47 +0530

    Add test file

---
+++ pages/HomePage.js
@@ -0,0 +1,44 @@
+import { expect } from '@playwright/test';
+export class HomePage {
+    constructor(page) {
+        this.page = page;
+        this.logo = page.locator('.nav-logo');
+        this.navlinks = page.locator('#navLinks'); 
+
+        this.describtion = page.locator('.hero-sub');
+        this.skillsGrid = page.locator('.skills-grid');
+        this.timeline = page.locator('.tl-date');
+        this.awsGrid = page.locator('.aws-grid');
+        this.toolsGrid = page.locator('.tool-item');
+        this.contactEmail = page.locator('.contact-email');
+    }
+    async logoIsVisible() {
+        await expect.soft(this.logo).toBeVisible();
+    }
+    async navlinksIsVisible() {
+        await expect.soft(this.navlinks).toBeVisible();
+    }
+    async describtionIsVisible() {
+        await expect.soft(this.describtion).toBeVisible();
+    }
+    async skillsGridIsVisible() {
+        await expect.soft(this.skillsGrid).toBeVisible();
+    }
+    async timelineIsVisible() {
+        await expect.soft(this.timeline).toBeVisible();
+        const timelineCount = await this.timeline.textContent();
+        await expect.soft(this.timeline).toHaveText('2022 - Now');
+    }
+    async awsGridIsVisible() {
+        await expect.soft(this.awsGrid).toBeVisible();
+    }
+    async toolsGridIsVisible() {
+        await expect.soft(this.toolsGrid).count().equal(16);
+    }
+    async contactEmailIsVisible() {
+        await expect.soft(this.contactEmail).toBeVisible();
+        const emailText = await this.contactEmail.textContent();
+        console.log(emailText);
+        await expect.soft(this.contactEmail).toHaveText('sealnirnay5@gmail.com');
+    }
+}
---
+++ tests/testRunner.spec.js
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+import { HomePage } from '../pages/HomePage';
+
+test('has title', async ({ page }) => {
+  await page.goto('http://54.197.109.37/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/QA Engineer Portfolio/);
+});
+
+test('home page should have all the sections visible', async ({ page }) => {
+    await page.goto('http://54.197.109.37/');
+    const homePage = new HomePage(page);
+    await homePage.logoIsVisible();
+    await homePage.navlinksIsVisible();
+    await homePage.describtionIsVisible();
+    await homePage.skillsGridIsVisible();
+    await homePage.timelineIsVisible();
+    await homePage.awsGridIsVisible();
+    await homePage.contactEmailIsVisible();
+});  
+
