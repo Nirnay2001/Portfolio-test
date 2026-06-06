194b8dfdd0bbb6afe7f4237c49a57d684660ba55
Nirnay2001 <154083662+Nirnay2001@users.noreply.github.com>
2026-06-07 00:02:02 +0530

    Update test URLs to new IP address

--- tests/testRunner.spec.js
+++ tests/testRunner.spec.js
@@ -2,14 +2,14 @@ import { test, expect } from '@playwright/test';
 import { HomePage } from '../pages/HomePage';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://100.24.10.145/');
+  await page.goto('http://3.84.207.36/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/QA Engineer Portfolio/);
 });
 
 test('home page should have all the sections visible', async ({ page }) => {
-    await page.goto('http://100.24.10.145/');
+    await page.goto('http://3.84.207.36/');
     const homePage = new HomePage(page);
     await homePage.logoIsVisible();
     await homePage.navlinksIsVisible();
